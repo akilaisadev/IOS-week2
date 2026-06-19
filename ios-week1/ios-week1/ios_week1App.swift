c206e6c058c0a445fd51400b79ff697f877dc71d
Akila Wasalathilaka <akilawasalathilaka@gmail.com>
2026-06-19 12:34:01 +0530

    project create

---
+++ ios-week1/ios-week1/ios_week1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_week1App.swift
+//  ios-week1
+//
+//  Created by Akila on 2026-06-19.
+//
+
+import SwiftUI
+
+@main
+struct ios_week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
