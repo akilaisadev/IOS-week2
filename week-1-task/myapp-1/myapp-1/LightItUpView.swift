f2eaa98746d835983fca34c92578ff0dd6366205
Akila Wasalathilaka <akilawasalathilaka@gmail.com>
2026-06-19 18:55:06 +0530

    layout improved with color

--- week-1-task/myapp-1/myapp-1/LightItUpView.swift
+++ week-1-task/myapp-1/myapp-1/LightItUpView.swift
@@ -2,15 +2,28 @@ import SwiftUI
 
 struct LightItUpView: View {
 
+    @State private var score = 0
+    @State private var activeCard = 4
+
     var body: some View {
 
         VStack(spacing: 20) {
 
             Text("Light It Up")
                 .font(.largeTitle)
+                .fontWeight(.bold)
 
-            Text("Score: 0")
-                .font(.title2)
+            HStack {
+
+                Text("Score: \(score)")
+                    .font(.title2)
+
+                Spacer()
+
+                Text("Time: 60")
+                    .font(.title2)
+            }
+            .padding(.horizontal)
 
             LazyVGrid(
                 columns: [
@@ -21,11 +34,18 @@ struct LightItUpView: View {
                 spacing: 15
             ) {
 
-                ForEach(0..<9, id: \.self) { _ in
+                ForEach(0..<9, id: \.self) { index in
 
                     RoundedRectangle(cornerRadius: 12)
-                        .fill(Color.gray.opacity(0.4))
+                        .fill(
+                            index == activeCard
+                            ? Color.blue
+                            : Color.gray.opacity(0.3)
+                        )
                         .frame(width: 90, height: 90)
+                        .scaleEffect(
+                            index == activeCard ? 1.1 : 1.0
+                        )
                 }
             }
 
