19967e63062375428183f30d75f9816fd29750ee
Akila Wasalathilaka <akilawasalathilaka@gmail.com>
2026-06-19 15:45:41 +0530

    Add score tracking functionality

--- week-1-task/myapp-1/myapp-1/ContentView.swift
+++ week-1-task/myapp-1/myapp-1/ContentView.swift
@@ -2,25 +2,27 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @State private var score = 0
+
     var body: some View {
 
-        VStack(spacing: 60) {
+        VStack(spacing: 30) {
 
             Text("Tap Frenzy")
                 .font(.largeTitle)
 
-            Text("Score: 0")
+            Text("Score: \(score)")
                 .font(.title)
 
             Text("Time: 10")
                 .font(.title2)
 
             Button("TAP") {
-
+                score += 1
             }
             .font(.largeTitle)
             .foregroundColor(.white)
-            .frame(width: 300, height: 300)
+            .frame(width: 200, height: 200)
             .background(Color.blue)
             .clipShape(Circle())
 
