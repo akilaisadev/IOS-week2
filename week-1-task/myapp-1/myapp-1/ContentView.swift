aeb6bfa2cc35ae7581c858ce479fa218f513592f
Akila Wasalathilaka <akilawasalathilaka@gmail.com>
2026-06-19 15:40:29 +0530

    create game layout

--- week-1-task/myapp-1/myapp-1/ContentView.swift
+++ week-1-task/myapp-1/myapp-1/ContentView.swift
@@ -1,19 +1,29 @@
-//
-//  ContentView.swift
-//  myapp-1
-//
-//  Created by Akila on 2026-06-19.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        VStack(spacing: 60) {
+
+            Text("Tap Frenzy")
+                .font(.largeTitle)
+
+            Text("Score: 0")
+                .font(.title)
+
+            Text("Time: 10")
+                .font(.title2)
+
+            Button("TAP") {
+
+            }
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .frame(width: 300, height: 300)
+            .background(Color.blue)
+            .clipShape(Circle())
+
         }
         .padding()
     }
