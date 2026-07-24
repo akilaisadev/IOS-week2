e310ed873eeb7849b63f8cd1240fd2e4142dfc52
Akila Wasalathilaka <akilawasalathilaka@gmail.com>
2026-07-24 18:52:42 +0530

    transform tap frenzy button into full vector bomb shape when skin is equipped

--- week-1-task/myapp-1/myapp-1/Components/Games/TapFrenzyBallView.swift
+++ week-1-task/myapp-1/myapp-1/Components/Games/TapFrenzyBallView.swift
@@ -13,33 +13,55 @@ struct TapFrenzyBallView: View {
     var body: some View {
         let activeIcon = MarketplaceService.shared.catalogue.first(where: { $0.id == activeSkinId })?.iconName ?? "hand.tap.fill"
         
-        VStack(spacing: 4) {
+        ZStack {
             if isTrapActive {
-                Image(systemName: "exclamationmark.octagon.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: buttonDiameter * 0.4, height: buttonDiameter * 0.4)
+                Circle()
+                    .fill(Color.red)
+                    .shadow(color: Color.red.opacity(0.5), radius: 15, x: 0, y: 8)
+                
+                VStack(spacing: 4) {
+                    Image(systemName: "exclamationmark.octagon.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: buttonDiameter * 0.4, height: buttonDiameter * 0.4)
+                        .foregroundColor(.white)
+                    
+                    Text("TRAP!")
+                        .font(.system(size: buttonDiameter * 0.18, weight: .black, design: .rounded))
+                        .foregroundColor(.white)
+                }
             } else {
-                CustomArtworkResolver(itemId: activeSkinId, iconName: activeIcon)
-                    .frame(width: buttonDiameter * 0.4, height: buttonDiameter * 0.4)
+                if activeSkinId == "skin_bomb" {
+                    BombVectorView()
+                        .frame(width: buttonDiameter, height: buttonDiameter)
+                        .shadow(color: Color.black.opacity(0.5), radius: 15, x: 0, y: 8)
+                    
+                    Text("TAP!")
+                        .font(.system(size: buttonDiameter * 0.18, weight: .black, design: .rounded))
+                        .foregroundColor(.white)
+                        .offset(y: buttonDiameter * 0.1) // slightly offset to sit on the belly of the bomb
+                } else {
+                    Circle()
+                        .fill(skinColor)
+                        .shadow(color: skinColor.opacity(0.5), radius: 15, x: 0, y: 8)
+                    
+                    VStack(spacing: 4) {
+                        CustomArtworkResolver(itemId: activeSkinId, iconName: activeIcon)
+                            .frame(width: buttonDiameter * 0.4, height: buttonDiameter * 0.4)
+                            .foregroundColor(.white)
+                        
+                        Text("TAP!")
+                            .font(.system(size: buttonDiameter * 0.18, weight: .black, design: .rounded))
+                            .foregroundColor(.white)
+                    }
+                }
             }
-            
-            Text(isTrapActive ? "TRAP!" : "TAP!")
-                .font(.system(size: buttonDiameter * 0.18, weight: .black, design: .rounded))
         }
-        .foregroundColor(.white)
         .frame(width: buttonDiameter, height: buttonDiameter)
-        .background(
-            Circle()
-                .fill(isTrapActive ? Color.red : skinColor)
-                .shadow(color: (isTrapActive ? Color.red : skinColor).opacity(0.5), radius: 15, x: 0, y: 8)
-        )
     }
     
     private var skinColor: Color {
-        if activeSkinId == "skin_bomb" {
-            return .black
-        } else if activeSkinId == "skin_lightning" {
+        if activeSkinId == "skin_lightning" {
             return .yellow
         } else {
             return .blue // default
